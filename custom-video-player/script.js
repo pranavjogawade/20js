f1581ba274088e86c73716c2e04fb267e2902b2e
Pranav Jogawade <pranavjogawade@localhost.localdomain>
2021-04-16 18:14:23 +0530

    video player added

---
+++ custom-video-player/script.js
@@ -0,0 +1,71 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+//Play and Pause
+function toggleVideoStatus() {
+    if(video.paused) {
+        video.play()
+    } else {
+        video.pause();
+    }
+}
+
+//update play pause icon
+function updatePlayIcon() {
+    if(video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+//update progress and timestamp
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    //Get min
+    let mins = Math.floor(video.currentTime / 60);
+    let secs = Math.floor(video.currentTime % 60);
+    if(mins < 10) {
+        mins = '0' + String(mins);
+    }
+    if(secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}: ${secs}`;
+
+}
+
+
+//set video time  progress
+function setVideoProgress() {
+    video.currentTime = Number(progress.value * video.duration) /100
+}
+
+//stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+
+
+
+
+
+//Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
